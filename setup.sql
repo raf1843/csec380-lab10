ec1b8b3c4ef339f575ef13c4ab8d96d8fe5ff7cd
raf1843 <raf1843@rit.edu>
2024-04-16 09:57:17 -0400

    pushing and removing source code

--- setup.sql
+++ setup.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE librenms CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'librenms'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON librenms.* TO 'librenms'@'localhost';
+CREATE USER 'librenms'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON librenms.* TO 'librenms'@'%';
